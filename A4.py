ccf82e7659590c0a72a1b288fa00ed929133a8b9
cc982 <cc982@cornell.edu>
2017-10-12 15:27:55 -0400

    Added .py version of A4

---
+++ A4.py
@@ -0,0 +1,27 @@
+import seaborn as sns
+import numpy as np
+import sklearn
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+housing_data = pd.DataFrame.from_csv('cleaned.csv')
+
+# Calculate the baseline for linear regression
+
+# Identify the relevant features
+housing_features = housing_data[:, []]
+
+#determine strength of correlation for features of interest
+X = pd.DataFrame(df,columns=['OverallQual', 'GarageCars', 'GarageArea', 'YearRemodAdd', 'YearBuilt', 'WoodDeckSF', 'OpenPorchSF', 'Lot Area', 'SalePrice']) 
+corr = np.corrcoef(X,rowvar=False) #rowvar = False b/c columns are features
+print (corr)
+
+# Create and test model for linear regression for 5 different train_test splits
+
+# Calculate the baseline for KNN model
+
+# Identify the relevant features
+
+# Create and test KNN model for 5 different train_test splits
